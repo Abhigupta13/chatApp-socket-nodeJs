ce1bcb0ad198311bd7e93c07ae62f97739e9372e
Abhigupta13 <abhishekkumargupta22110@gmail.com>
2023-07-06 19:39:56 +0530

    Added codes

--- index.js
+++ index.js
@@ -9,13 +9,13 @@ const io = socketio(server);
 io.on('connection', (socket) => {
     console.log('a user connected', socket.id);
 
-    socket.on('from_client', () => {
-        console.log("event coming from client");
+    socket.on('msg_send', (data) => {
+        console.log(data);
+        // io.emit('msg_rcvd', data);
+        // socket.emit('msg_rcvd', data)    // shows msg on same browser
+        socket.broadcast.emit('msg_rcvd', data)   // shows msg on every active user except current sender
     })
 
-    setInterval(() => {
-        socket.emit('from_server');
-    }, 2000);
 });
 
 app.use('/', express.static(__dirname + '/public'));
