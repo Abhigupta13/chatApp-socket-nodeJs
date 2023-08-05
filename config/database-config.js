abeecc92a0088cfa99f94fe37707ecef8ee87ceb
Abhigupta13 <abhishekkumargupta22110@gmail.com>
2023-08-05 16:36:04 +0530

    shift compass to atlas

--- config/database-config.js
+++ config/database-config.js
@@ -1,7 +1,20 @@
-const mongoose = require('mongoose');
-
+const mongoose = require('mongoose')
+const password = encodeURIComponent("Akg@13579");
 const connect = async () => {
-    await mongoose.connect("mongodb://0.0.0.0:27017/chatapp");
-}
+    try {
+        const connect = await mongoose.connect(
+            `mongodb+srv://AKGROCKX:${password}@cluster-akg.qw0qmd8.mongodb.net/chatapp`
+            , {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        })
+        console.log(`MongoDB Connected : ${connect.connection.host}`)
+    } catch (error) {
+        console.log(error);
+        process.exit();
+
+    }
+};
 
 module.exports = connect;
+//mongodb+srv://DJ0212:devansh%400212@cluster0.djsnamb.mongodb.net
